5a3de94a745c180ef70b285c7817f95fe9f19252
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2024-05-30 15:13:52 +0700

    Merge reading to develop

---
+++ examples/nextjs-dashboard/app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,34 @@
+import { Metadata } from 'next';
+
+// Helpers
+import { fetchCustomers } from '@/app/lib/data';
+
+// Components
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+
+export const metadata: Metadata = {
+  title: 'Create Invoice',
+};
+
+const Page = async () => {
+  const customers = await fetchCustomers();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+};
+
+export default Page;
---
+++ examples/nextjs-dashboard/app/ui/search.tsx
@@ -0,0 +1,49 @@
+'use client';
+// Libs
+import { ChangeEvent } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
+
+// Components
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+
+// Hooks
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+const Search = ({ placeholder }: { placeholder: string }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault();
+      const value = e.target.value;
+      const params = new URLSearchParams(searchParams);
+      params.set('page', '1');
+
+      if (value) {
+        params.set('query', value);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString()}`);
+    },
+  );
+
+  return (
+    <div className="relative flex flex-1 flex-shrink-0">
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <input
+        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        placeholder={placeholder}
+        onChange={handleSearch}
+        defaultValue={searchParams.get('query')?.toString()}
+      />
+      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+    </div>
+  );
+};
+
+export default Search;
