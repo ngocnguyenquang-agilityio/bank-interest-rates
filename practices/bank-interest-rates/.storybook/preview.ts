76f286c37f85f15b0ad1e1f052d8cbefc7db0653
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2024-05-31 09:07:46 +0700

    Feat(#54): config linter

---
+++ practices/bank-interest-rates/.storybook/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from "@storybook/react";
+import "../src/app/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    nextjs: { appDirectory: true },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
